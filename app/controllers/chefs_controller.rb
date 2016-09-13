285d046e4e9e358e0bdee483f24c62a8950bee32
Carlos Ferreira <carlosprovisorio@icloud.com>
2016-09-13 11:57:35 +0000

    Created edit feature

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -15,11 +15,17 @@ class ChefsController < ApplicationController
    end
    
    def edit
-     
+     @chef = Chef.find(params[:id])
    end
    
    def update
-   
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your profile has been updated succesfully"
+      redirect_to recipes_path #TODOchange to show chef page
+    else
+      render 'edit'
+    end
    end
    
    private
