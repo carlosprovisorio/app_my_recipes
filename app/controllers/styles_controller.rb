ae5a99498fa2cb17b4df86a0adae3bfeb0adf5b9
Carlos Ferreira <carlosprovisorio@icloud.com>
2016-09-22 01:32:39 +0000

    Added create view for ingredients, created form for ingredients, crated method for create under ingredients controller

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -5,6 +5,19 @@ class StylesController < ApplicationController
   end
   
   def create
+    @style = Style.new(style_params)
+    if @style.save
+      flash[:success] = "Style was created succesfully"
+      redirect_to recipes_path
+    else
+      render "new"
+    end
+  end
+  
+  private
+  
+  def style_params
+    params.require(:style).permit(:name)
     
   end
 end
