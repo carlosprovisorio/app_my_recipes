7cf23a3971d3fbf28a27377682290f2c54be63e0
Carlos Ferreira <carlosprovisorio@icloud.com>
2016-10-03 13:22:18 +0000

    Display styles and ingredients, set up dropdown for style and ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,7 +2,9 @@ class IngredientsController < ApplicationController
    
    def show
       @ingredient = Ingredient.find(params[:id])
+      @recipes = @ingredient.recipes.paginate(page: params[:page], per_page: 4)
    end
+   
    
    def new
      @ingredient = Ingredient.new
